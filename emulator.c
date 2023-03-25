42be41dde8510838609b6c331d5cb17b47684488
Filadelphila <julien.combrexelle@gmail.com>
2023-03-25 13:12:52 +0100

    refactor: create disassemble() function

--- emulator.c
+++ emulator.c
@@ -2,9 +2,41 @@
 #include <SDL2/SDL.h>
 #include "emulator/chip8.h"
 #include "log.h"
+#include "disassembler.h"
+#include <libgen.h>
 
 bool debug = false;
-void disassemble(char *file);
+
+int disassemble(char *file) {
+    FILE *rom = fopen(file, "rb");
+    if (!rom) {
+        fprintf(stderr, "%s not found", file);
+        return 1;
+    }
+
+    char *asm_file;
+    asm_file = malloc(strlen(basename(file)) + 4);
+    snprintf(asm_file, strlen(basename(file)) + 5, "%s.asm", basename(file));
+    FILE *assembly = fopen(asm_file, "w");
+    if (!assembly) {
+        fprintf(stderr, "failed to open %s", asm_file);
+        return 1;
+    }
+    long rom_size;
+    uint8_t *binary = get_rom(rom, &rom_size);
+    for (int i = 0; i < rom_size + 1; i += 2) {
+        uint16_t opcode = (binary[i] << 8) | binary[i + 1];
+        char *instruction = get_asm_code(opcode);
+        if (strlen(instruction) > 0) {
+            fprintf(assembly, "%s\n", instruction);
+        } else {
+            fprintf(assembly, "0x%04X\n", opcode);
+        }
+    }
+    free(binary);
+    fclose(assembly);
+}
+
 int main(int argc, char *argv[]) {
     chip8_init();
     for (int arg = 1; arg < argc; arg++) {
@@ -14,6 +46,7 @@ int main(int argc, char *argv[]) {
             debug = true;
         } else if ((strcmp(argv[arg], "--disassemble") == 0) && (arg + 1) < argc) {
             printf("%d %d disassemble to [%s]\n", arg, argc, argv[arg + 1]);
+            disassemble(argv[arg + 1]);
             continue;
         } else if (!chip8_load_rom(argv[arg])) {
             fprintf(stderr, "rom %s not found\n", argv[arg]);
